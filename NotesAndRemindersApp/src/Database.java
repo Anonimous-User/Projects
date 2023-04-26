c47ef56247870fe3dee720865a824a5d23ba0b5a
anonimous-user <harrisbond1234@gmail.com>
2023-04-26 13:53:08 -0400

    tried to work on database a bit more

--- NotesAndRemindersApp/src/Database.java
+++ NotesAndRemindersApp/src/Database.java
@@ -6,6 +6,13 @@ import java.sql.*;
 
 public class Database {
 
+	enum types{
+		Time,
+		Reminder,
+		Note,
+		SpecializedProtein
+	}
+
     private static Connection connection;
     
 	public static void main(String[] args) throws SQLException, ClassNotFoundException {
@@ -17,14 +24,51 @@ public class Database {
 			 	prep.executeQuery();
 		  	}
 
-			
-		} finally{
+		}
+		catch(SQLException e){
+			System.out.println(e);
+		}
+		finally{
 			try (PreparedStatement prep = connection.prepareStatement(
                   "DELETE FROM test")) {
                prep.executeQuery();
             }
 		}
 	}
+
+	public static void insertData(types column, String msg) throws SQLException{
+		try(PreparedStatement statement = connection.prepareStatement(
+				"INSERT INTO testuser(" + column + ") VALUES (" + msg +")"
+			)) {
+				statement.executeQuery();
+		}
+	}
+
+	public static void retrieveDate(types column){
+
+	}
+	// use as reference
+	// private static void readData() throws SQLException {
+	// 	try (PreparedStatement statement = connection.prepareStatement("""
+	// 			    SELECT name, rating
+	// 			    FROM programming_language
+	// 			    ORDER BY rating DESC
+	// 			""")) {
+	// 		try (ResultSet resultSet = statement.executeQuery()) {
+	// 			boolean empty = true;
+	// 			while (resultSet.next()) {
+	// 				empty = false;
+	// 				String name = resultSet.getString("name");
+	// 				int rating = resultSet.getInt("rating");
+	// 				System.out.println("\t> " + name + ": " + rating);
+	// 			}
+	// 			if (empty) {
+	// 				System.out.println("\t (no data)");
+	// 			}
+	// 		}
+	// 	}
+	// }
+
 
 	private static void initDatabaseConnection() throws SQLException {
 		System.out.println("Connecting to the database...");
