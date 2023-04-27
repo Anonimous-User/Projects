5de4bf9efc24c0cfae0b6e7f7d713415b2742d64
Anonimous-User <95382802+Anonimous-User@users.noreply.github.com>
2023-04-27 02:36:19 -0400

    retrieving data and inserting data now works. Table has also been created for testuser

--- NotesAndRemindersApp/src/Database.java
+++ NotesAndRemindersApp/src/Database.java
@@ -2,15 +2,15 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.sql.*;
 
 public class Database {
 
-	enum types{
+	public static enum types{
 		Time,
 		Reminder,
-		Note,
-		SpecializedProtein
+		Note
 	}
 
     private static Connection connection;
@@ -18,56 +18,58 @@ public class Database {
 	public static void main(String[] args) throws SQLException, ClassNotFoundException {
 		try {
 			initDatabaseConnection();
-			try (PreparedStatement prep = connection.prepareStatement(
-				"INSERT INTO test(testColomb) VALUES (100)"
-				)) {
-			 	prep.executeQuery();
-		  	}
-
+			// try (PreparedStatement prep = connection.prepareStatement(
+			// 	"CREATE TABLE testuser(Note TEXT, Reminder TEXT, Time TEXT)"
+			// 	)) {
+			//  	prep.executeQuery();
+		  	// }
+			// insertData(types.Note, "testtest");
+			retrieveData();
 		}
 		catch(SQLException e){
 			System.out.println(e);
 		}
 		finally{
-			try (PreparedStatement prep = connection.prepareStatement(
-                  "DELETE FROM test")) {
-               prep.executeQuery();
-            }
+			// try (PreparedStatement prep = connection.prepareStatement(
+            //       "DELETE FROM test")) {
+            //    prep.executeQuery();
+            // }
 		}
 	}
 
 	public static void insertData(types column, String msg) throws SQLException{
 		try(PreparedStatement statement = connection.prepareStatement(
-				"INSERT INTO testuser(" + column + ") VALUES (" + msg +")"
+				"INSERT INTO testuser(" + column + ") VALUES ('" + msg +"')"
 			)) {
 				statement.executeQuery();
 		}
 	}
 
-	public static void retrieveDate(types column){
+	public static ArrayList<String[]> retrieveData() throws SQLException{
+		try(PreparedStatement statement = connection.prepareStatement(
+			"SELECT Note, Reminder, Time FROM testuser"
+		)){
+			try (ResultSet resultSet = statement.executeQuery()) {
+				boolean empty = true;
+				int i=0;
+				ArrayList<String[]> rtn = new ArrayList<String[]>();
+				while (resultSet.next()) {
+					empty = false;
+					rtn.add(new String[3]);
+					rtn.get(i)[0] = resultSet.getString("Note");
+					rtn.get(i)[1] = resultSet.getString("Reminder");
+					rtn.get(i)[2] = resultSet.getString("Time");
 
+					System.out.println("\t> " + rtn.get(i)[0] + " : " + rtn.get(i)[1] + " : " + rtn.get(i)[2]);
+					i++;
+				}
+				if (empty) {
+					System.out.println("\t (no data)");
+				}
+				return rtn;
+			}
+		}
 	}
-	// use as reference
-	// private static void readData() throws SQLException {
-	// 	try (PreparedStatement statement = connection.prepareStatement("""
-	// 			    SELECT name, rating
-	// 			    FROM programming_language
-	// 			    ORDER BY rating DESC
-	// 			""")) {
-	// 		try (ResultSet resultSet = statement.executeQuery()) {
-	// 			boolean empty = true;
-	// 			while (resultSet.next()) {
-	// 				empty = false;
-	// 				String name = resultSet.getString("name");
-	// 				int rating = resultSet.getInt("rating");
-	// 				System.out.println("\t> " + name + ": " + rating);
-	// 			}
-	// 			if (empty) {
-	// 				System.out.println("\t (no data)");
-	// 			}
-	// 		}
-	// 	}
-	// }
 
 
 	private static void initDatabaseConnection() throws SQLException {
